f4a658e261c32a3c4963bb628f13f0ee3a7233d7
Chaitrika Budamagunta <cbudamagunta@dons.usfca.edu>
2020-05-10 15:59:53 -0700

    Start bar chart implementation

--- childhoodMap.js
+++ childhoodMap.js
@@ -19,14 +19,18 @@ const g = {
   basemap: svg.select("g#basemap"),
   states: svg.select("g#states"),
   details: svg.select("g#details"),
-  legend: svg.select("g#legend")
+  legend: svg.select("g#legend"),
+
+  bar: svg.select("g#bar"),
+  barX: svg.select("g#barX"),
+  barY: svg.select("g#barY")
 };
 
 
 /* Details widgit */
 const details = g.details.append("foreignObject")
   .attr("id", "details")
-  .attr("width", 325)
+  .attr("width", 200)
   .attr("height", 600)
   .attr("x", 20)
   .attr("y", 10);
@@ -255,6 +259,12 @@ function drawBasemap(json, data) {
     /* Interactivity */
     states.on("mouseover", function(d) {
 
+      /* Draw Bar Chart*/
+      drawBarChart(d, data);
+      d3.select("g#bar").style("visibility", "visible");
+      d3.select("g#barX").style("visibility", "visible");
+      d3.select("g#barY").style("visibility", "hidden");
+
       /* Highlight Neighborhoods */
       d3.select(this).raise().classed("active", true);
 
@@ -282,7 +292,32 @@ function drawBasemap(json, data) {
       body.html(html);
       details.style("visibility", "visible");
     })
+    .on("mousemove", function(d) {
+      /* Draw Bar Chart*/
+      d3.select("g#bar").selectAll("text").remove();
+      d3.select("g#bar").selectAll("rect").remove();
+
+      d3.select("g#barX").selectAll("text").remove();
+      d3.select("g#barX").selectAll("x-axis-barline").remove();
+
+      d3.select("g#barY").selectAll("text").remove();
+      d3.select("g#barY").selectAll("y-axis-barline").remove();
+
+
+
+      drawBarChart(d, data);
+    })
     .on("mouseout", function(d) {
+
+      /* Draw Bar Chart*/
+      d3.select("g#bar").selectAll("text").remove();
+      d3.select("g#bar").selectAll("rect").remove();
+
+      d3.select("g#barX").selectAll("text").remove();
+      d3.select("g#barX").selectAll("x-axis-barline").remove();
+
+      d3.select("g#barY").selectAll("text").remove();
+      d3.select("g#barY").selectAll("y-axis-barline").remove();
 
       /* Highlight Neighborhoods */
       d3.select(this).lower().classed("active", false);
@@ -291,6 +326,219 @@ function drawBasemap(json, data) {
       details.style("visibility", "hidden");
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+* BAR CHART
+*/
+
+/* Draw the Bar Chart */
+function drawBarChart(state, data) {
+
+  let dataMatch = data.filter(e => e.state === state.properties.name);
+  let barGroups = [
+    "inventor",
+    "inventorPQ1",
+    "inventorPQ2",
+    "inventorPQ3",
+    "inventorPQ4",
+    "inventorPQ5",
+    "male",
+    "female"
+  ];
+
+  let dataMax = dataMatch[0].inventor;
+  let dataMin = 0;
+
+  /* Set Up Plot */
+  const barPlot = g.bar
+    .attr("id", "bar")
+    .attr("transform", "translate(300,80)");
+
+  const barScales = {
+    x: d3.scaleBand(),
+    y: d3.scaleLinear(),
+  };
+
+  let barPlotWidth = 300;
+  let barPlotHeight = 50;
+
+  barScales.x.range([0, 100]);
+
+  barScales.y
+      .domain([dataMin, dataMax])
+      .range([barPlotHeight, 0])
+      .nice();
+
+  let yGroup = g.barY.attr("id", "y-axis-barline").attr('class', 'axis');
+  let yAxis = d3.axisLeft(barScales.y);
+
+  yAxis.ticks(5, '0.3%').tickSizeOuter(0);
+  yGroup.call(yAxis);
+
+
+  /* Draw Titles */
+  // let xMiddle = 300 + midpoint(barScales.x.range());
+  // let yMiddle = 20 + midpoint(barScales.y.range());
+  //
+  // let yTitle = barPlot.append('text')
+  //   .attr('transform', translate(250, yMiddle))
+  //   .attr('class', 'axis-title')
+  //   .attr('id', 'axis-title')
+  //   .text('Inventor Rate');
+  //
+  // yTitle.attr('x', -50);
+  // yTitle.attr('y', -50);
+  //
+  // yTitle.attr('dy', 15);
+  // yTitle.attr('text-anchor', 'middle');
+  // yTitle.attr('transform', 'rotate(-90)');
+
+
+  /* DRAW AXIS */
+  barScales.x.domain(barGroups);
+
+  let xGroup = g.barX.attr("id", "x-axis-barline").attr('class', 'axis');
+  let xAxis = d3.axisBottom(barScales.x).tickPadding(0).tickSizeOuter(0);
+
+  xGroup.attr('transform', translate(300, barPlotHeight));
+  xGroup.call(xAxis)
+    .selectAll("text")
+      .attr("y", 4)
+      .attr("x", -5)
+      .attr("dy", ".35em")
+      .attr("transform", "rotate(-90)")
+      .style("text-anchor", "end");
+
+
+  /* Set Up Bar Data */
+  const barData = [
+    {
+      group: "inventor",
+      value: dataMatch[0].inventor
+    },
+    {
+      group: "inventorPQ1",
+      value: dataMatch[0].inventorPQ1
+    },
+    {
+      group: "inventorPQ2",
+      value: dataMatch[0].inventorPQ2
+    },
+    {
+      group: "inventorPQ3",
+      value: dataMatch[0].inventorPQ3
+    },
+    {
+      group: "inventorPQ4",
+      value: dataMatch[0].inventorPQ4
+    },
+    {
+      group: "inventorPQ5",
+      value: dataMatch[0].inventorPQ5
+    },
+    {
+      group: "male",
+      value: dataMatch[0].male
+    },
+    {
+      group: "female",
+      value: dataMatch[0].female
+    },
+  ];
+
+
+  /* Bar Chart */
+  const bars = g.bar
+    .selectAll("rect")
+    .attr("id", "bars")
+    .data(barData)
+    .enter()
+    .append("rect")
+      .attr("class", d => d.group)
+      .attr("x", d => (barScales.x(d.group) + (barScales.x.bandwidth() / 2)))
+      .attr("y", d => barScales.y(d.value))
+      .attr("width", barScales.x.bandwidth() - 3)
+      .attr("height", d => barPlotHeight - barScales.y(d.value))
+      // .style("fill", "#a3c7e1");
+
+    // .append("rect")
+    //   .attr("x", (barScales.x("inventor") + (barScales.x.bandwidth() / 2)))
+    //   .attr("y", barScales.y(dataMatch[0].inventor))
+    //   .attr("width", barScales.x.bandwidth() - 3)
+    //   .attr("height", barPlotHeight - barScales.y(dataMatch[0].inventor));
+    //
+    // .append("rect")
+    //   .attr("x", (barScales.x("inventorPQ1") + (barScales.x.bandwidth() / 2)))
+    //   .attr("y", barScales.y(dataMatch[0].inventorPQ1))
+    //   .attr("width", barScales.x.bandwidth() - 3)
+    //   .attr("height", barPlotHeight - barScales.y(dataMatch[0].inventorPQ1))
+    //
+    // .append("rect")
+    //   .attr("x", (barScales.x("inventorPQ2") + (barScales.x.bandwidth() / 2)))
+    //   .attr("y", barScales.y(dataMatch[0].inventorPQ2))
+    //   .attr("width", barScales.x.bandwidth() - 3)
+    //   .attr("height", barPlotHeight - barScales.y(dataMatch[0].inventorPQ2))
+    //
+    // .append("rect")
+    //   .attr("x", (barScales.x("inventorPQ3") + (barScales.x.bandwidth() / 2)))
+    //   .attr("y", barScales.y(dataMatch[0].inventorPQ3))
+    //   .attr("width", barScales.x.bandwidth() - 3)
+    //   .attr("height", barPlotHeight - barScales.y(dataMatch[0].inventorPQ3))
+    //
+    // .append("rect")
+    //   .attr("x", (barScales.x("inventorPQ4") + (barScales.x.bandwidth() / 2)))
+    //   .attr("y", barScales.y(dataMatch[0].inventorPQ4))
+    //   .attr("width", barScales.x.bandwidth() - 3)
+    //   .attr("height", barPlotHeight - barScales.y(dataMatch[0].inventorPQ4))
+    //
+    // .append("rect")
+    //   .attr("x", (barScales.x("inventorPQ5") + (barScales.x.bandwidth() / 2)))
+    //   .attr("y", barScales.y(dataMatch[0].inventorPQ5))
+    //   .attr("width", barScales.x.bandwidth() - 3)
+    //   .attr("height", barPlotHeight - barScales.y(dataMatch[0].inventorPQ5))
+    //
+    // .append("rect")
+    //   .attr("x", (barScales.x("male") + (barScales.x.bandwidth() / 2)))
+    //   .attr("y", barScales.y(dataMatch[0].male))
+    //   .attr("width", barScales.x.bandwidth() - 3)
+    //   .attr("height", barPlotHeight - barScales.y(dataMatch[0].male))
+    //
+    // .append("rect")
+    //   .attr("x", (barScales.x("female") + (barScales.x.bandwidth() / 2)))
+    //   .attr("y", barScales.y(dataMatch[0].female))
+    //   .attr("width", barScales.x.bandwidth() - 3)
+    //   .attr("height", barPlotHeight - barScales.y(dataMatch[0].female));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
@@ -314,6 +562,9 @@ function parseData(row){
   keep.state = row["par_state"];
   keep.stateAbbrv = row["par_stateabbrv"];
 
+  keep.male = row["inventor_g_m"];
+  keep.female = row["inventor_g_f"];
+
   return keep;
 }
 
@@ -321,6 +572,14 @@ function parseData(row){
 /*
 * HELPER FUNCTIONS
 */
+
+/*
+ * From bubble.js example:
+ * calculates the midpoint of a range given as a 2 element array
+ */
+function midpoint(range) {
+  return range[0] + (range[1] - range[0]) / 2.0;
+}
 
 /*
  * From bubble.js example:
